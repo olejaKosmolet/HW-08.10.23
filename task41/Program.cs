c08ec6a7824f18bc96ffaef50ea5886f6484c9e7
oleja_kosmolet <olegermolaev45@gmail.com>
2023-10-08 11:39:01 +0300

    added task41

---
+++ task41/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+int[] MakeArray1()
+{
+    Console.WriteLine("Введите числа через пробел: ");
+    int[] number = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+    return number;
+}
+
+void MakeCount1(int[] number)
+{
+    int count = 0;
+    for (int i = 0; i < number.Length; i++)
+    {
+        if (number[i] > 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine(count);
+}
+
+
+MakeCount1(MakeArray1());
+
